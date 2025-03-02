8f8f9fcdee3715ad2f715a70c68bb20e5989dea2
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-03-02 22:37:53 +0530

    cron route

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,11 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func LoadRoutes(r *gin.Engine) {
+	r.GET("/home", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
 	roomRoutes := r.Group("/room")
 	fileRoutes := r.Group("/file")
 	userRoutes := r.Group("/user")
